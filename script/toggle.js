328f959802c9c78eace71cdb308be9d749b8b03a
abdullah01846 <mdabdullahh106@gmail.com>
2025-11-04 16:22:41 +0600

    project cashout section done

---
+++ script/toggle.js
@@ -0,0 +1,10 @@
+document.getElementById('add-money-box').addEventListener('click' , function(){
+    console.log('dd')
+    document.getElementById('casout').style.display = 'none'
+    document.getElementById('addMoney').style.display = 'block'
+})
+document.getElementById('cashout-box').addEventListener('click' , function(){
+    console.log('dd')
+    document.getElementById('casout').style.display = 'block'
+    document.getElementById('addMoney').style.display = 'none'
+})
